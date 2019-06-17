ddd2922ab410abff927470ee27fabccf30a2d605
Alexander Thomas <cawfree@Alexanders-MacBook-Pro.local>
2019-06-17 12:25:58 +0100

    🚀 published to github

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # react-native-cycle-text
 A simple React Native component which lets you randomly cycle through an array of text Strings.
+
+It is compatible with [react-native-web](https://github.com/necolas/react-native-web), and also has built-in hyperlink support!
+
+## 🚀 Getting Started
+Using [npm](https://www.npmjs.com/package/@cawfree/react-native-cycle-text):
+```
+npm install --save @cawfree/react-native-cycle-text
+```
+
+Using [yarn](https://www.npmjs.com/package/@cawfree/react-native-cycle-text):
+```
+yarn add @cawfree/react-native-cycle-text
+```
+
+## ✍️ Example
+```
+import TextCycler from '@cawfree/react-native-cycle-text';
+
+export default ({ ...extraProps }) => (
+  <TextCycler
+    onPress={(url) => {
+      // Returned if a user tapped on a hyperlink.
+    }}
+    duration={2000}
+    nextMessage={() => {
+      return Math.random() > 0.5 ? [
+        'https://github.com/Cawfree are now welcome!',
+        'Pull requests',
+      ] : [
+        'There\'s no better feeling than when somebody contributes to your repo on https://github.com/Cawfree.',
+        'GitHub',
+      ];
+    }}
+  />
+);
+
+```
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Animated,
+} from 'react-native';
+import Hyperlink from 'react-native-hyperlink';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create(
+  {
+    messageStyle: {
+      textAlign: 'justify',
+      color: '#FFFFFFAA',
+    },
+  },
+);
+
+class TextCycler extends React.Component {
+  constructor(nextProps) {
+    super(nextProps);
+    this.state = ({
+      animOpacity: null,
+      message: nextProps.nextMessage(),
+    });
+    this.__cycle = this.__cycle.bind(this);
+    this.__getMessageLinkText = this.__getMessageLinkText.bind(this);
+  }
+  componentDidMount() {
+    const {
+      disabled,
+    } = this.props;
+    if (!disabled) {
+      this.__startCycle();
+    }
+  }
+  componentWillUpdate(nextProps, nextState) {
+    if (!nextProps.disabled && this.props.disabled) {
+      this.__startCycle();
+    }
+  }
+  componentWillUnmount() {
+    this.setState({
+      animOpacity: null,
+    });
+  }
+  __startCycle() {
+    const {
+      disabled,
+    } = this.props;
+    const {
+      animOpacity,
+    } = this.state;
+    if (!animOpacity && !disabled) {
+      this.setState(
+        {
+          animOpacity: new Animated.Value(0),
+        },
+        this.__cycle,
+      );
+    }
+  }
+  __cycle() {
+    const {
+      nextMessage,
+      duration,
+    } = this.props;
+    const {
+      animOpacity,
+    } = this.state;
+    if (animOpacity) {
+      Animated.sequence(
+        [
+          Animated.timing(
+            animOpacity,
+            {
+              toValue: 1,
+              duration: 600,
+              useNativeDriver: true,
+            },
+          ),
+          Animated.timing(
+            animOpacity,
+            {
+              toValue: 1,
+              duration,
+              useNativeDriver: true,
+            }
+          ),
+          Animated.timing(
+            animOpacity,
+            {
+              toValue: 0,
+              duration: 600,
+              useNativeDriver: true,
+            },
+          ),
+        ],
+      ).start(() => setTimeout(
+        () => {
+          this.setState(
+            {
+              message: nextMessage(),
+            },
+            this.__cycle,
+          );
+        },
+        300,
+      ));
+    }
+  }
+  __getMessageLinkText() {
+    const {
+      message,
+    } = this.state;
+    return message[1];
+  }
+  render() {
+    const {
+      style,
+      linkStyle,
+      duration,
+      disabled,
+      onPress,
+      messageStyle,
+      linkStyle,
+    } = this.props;
+    const {
+      animOpacity,
+      message,
+    } = this.state;
+    return (
+      <Animated.View
+        style={[
+          style,
+          {
+            opacity: (animOpacity || 0),
+          },
+        ]}
+      >
+        <Hyperlink
+          onPress={onPress}
+          linkStyle={linkStyle}
+          linkText={this.__getMessageLinkText}
+        >
+          <Text
+            style={messageStyle}
+          >
+            {message[0]}
+          </Text>
+        </Hyperlink>
+      </Animated.View>
+    );
+  }
+}
+
+TextCycler.propTypes = {
+  style: PropTypes.shape({}),
+  linkStyle: PropTypes.shape({}),
+  duration: PropTypes.number,
+  disabled: PropTypes.bool,
+  onPress: PropTypes.func,
+  messageStyle: PropTypes.shape({}).isRequired,
+  linkStyle: PropTypes.shape({}).isRequired,
+};
+
+TextCycler.defaultProps = {
+  style: {},
+  linkStyle: styles.linkStyle,
+  duration: 3000,
+  disabled: false,
+  onPress: e => null,
+  messageStyle: styles.messageStyle,
+  linkStyle: {},
+};
+
+export default TextCycler;
